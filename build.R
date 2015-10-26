32d2104c682b1e5249dc9f89d6e563d4b68fa3f4
Charles Ravarani <c.ravarani@gmail.com>
2015-10-26 18:18:59 +0000

    mainly fixing css for code line numbering

--- build.R
+++ build.R
@@ -6,7 +6,7 @@ local({
   markdown = servr:::jekyll_config('.', 'markdown', 'kramdown')
   # see if we need to use the Jekyll render in knitr
   if (markdown == 'kramdown') {
-    knitr::render_jekyll(highlight = "pygments", extra = "linenos")
+    knitr::render_jekyll(highlight = "pygments", extra = "lineanchors linenos=table")
   } else knitr::render_markdown()
 
   # input/output filenames are passed as two additional arguments to Rscript
